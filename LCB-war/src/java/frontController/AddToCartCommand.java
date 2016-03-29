b22623ff88ee9cb78283eb29d9def19f115c413f
Javi <jmoranrod@gmail.com>
2016-03-29 09:16:33 +0100

    AddToCartFixed

--- LCB-war/src/java/frontController/AddToCartCommand.java
+++ LCB-war/src/java/frontController/AddToCartCommand.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package frontController;
 
 import controller.BookFacadeLocal;
 import entity.Book;
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.InitialContext;
@@ -22,30 +16,59 @@ import util.ShoppingCart;
  * @author maxi
  */
 public class AddToCartCommand extends FrontCommand{
-    
-    
+
+
     @Override
     public void process() {
-        
+
         try {
             BookFacadeLocal books;
             HttpSession session = request.getSession();
             ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
             Integer isbn = Integer.parseInt(request.getParameter("bookIsbn"));
+//            Integer isbn = Integer.parseInt(request.getParameter("bookIsbn"));
             books = InitialContext.doLookup("java:global/LCB/LCB-ejb/BookFacade");
-            List<Book> bookList = books.findAll();
-            //Tratarlo como HashMap<Book, Integer>, donde integer es el numero 
+            //Tratarlo como HashMap<Book, Integer>, donde integer es el numero
             //de copias/contador de los libros que se pueden añadir al carrito
-            //cuando llegue a 0, cambiar el botón por reservar, o que no siga 
+            //cuando llegue a 0, cambiar el botón por reservar, o que no siga
             //metiendo mas en el carrito
+
+            Book bookDB = books.find(isbn);
+            if(bookDB != null){
+                if(cart.getCart().isEmpty()){
+                    cart.addBoookToCart(bookDB);
+                }else{
+                    int index = -1;
+                    for (int i = 0; i < cart.getCart().size(); i++) {
+                        if(cart.getCart().get(i).getIsbn().equals(isbn)){
+                            index = i;
+                        }
+                    }
+                    if(index == -1){
+                        cart.addBoookToCart(bookDB);
+                    }else{
+                        Book modBook = cart.getCart().get(index);
+                        Book chachiBook = (Book) modBook.clone();
+                        chachiBook.setCopy(modBook.getCopy() - 1);
+                        if(chachiBook.getCopy() > 0)
+                            cart.addBoookToCart(chachiBook);
+                    }
+                }
+            }
+            /*
+
+            List<Book> bookList = books.findAll();
             for (Book book : bookList) {
                 if (book.getIsbn().equals(isbn)) {
                     cart.addBoookToCart(book);
                     //session.setAttribute("cart", cart);
                 }
             }
+*/
             forward("/indexView.jsp");
         } catch (ServletException | IOException | NamingException ex) {
+            Logger.getLogger(AddToCartCommand.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (CloneNotSupportedException ex) {
             Logger.getLogger(AddToCartCommand.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
